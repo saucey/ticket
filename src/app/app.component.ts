9518d8ec717f0485c38f77752b18bdd05f47b54e
Leo Scott <leoscott@Leos-MacBook-Pro.local>
2021-05-13 10:10:37 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { ApiTicketService } from './ticket.service';
+import { TrainRoot } from './tickets.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'tickets';
+  public url = '/assets/json/tickets.json';
+  public tickets$: Observable<TrainRoot[]> = this.api.getListOfTicket(this.url);
+
+  constructor(private api: ApiTicketService) {}
 }
